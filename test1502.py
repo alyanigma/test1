33b1f3f4f86d82fe2075a06295e96c2d8c166744
alyanigma <alyanov1312@gmail.com>
2023-09-11 20:35:57 +0700

    y

--- test1502.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-data = pd.read_excel("lab_pi_101.xlsx")
-#idx = pd.Index(data)
-#idx.unique()
-#idx.count = ()
-#print(idx.value_counts)
-data = data.drop_duplicates (subset=['col1', 'col2'])
-print(data)
