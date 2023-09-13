1fbc5833c8ff515200169e54d2cab532de9a7793
alyanigma <alyanov1312@gmail.com>
2023-09-13 18:28:25 +0700

    y

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import pandas as pd
 #
-lipa = pd.read_excel("lab_pi_101.xlsx")
-CEst = lipa.shape[0]
-CEst1 = lipa['Группа'].str.contains('ПИ101').sum()
+maindata = pd.read_excel("lab_pi_101.xlsx")
+CEst = maindata.shape[0]
+CEst1 = maindata['Группа'].str.contains('ПИ101').sum()
 #CEst = 0
-#for index, row in lipa.iterrows():
+#for index, row in maindata.iterrows():
     #if any(map(str.isdigit, str(row['Сокращенная оценка']))):
         #CEst += 1
-#CEst1 = len(lipa[(lipa['Сокращенная оценка'].astype(str).str.isnumeric()) & (lipa['Группа'] == 'ПИ101')])
+#CEst1 = len(maindata[(lipa['Сокращенная оценка'].astype(str).str.isnumeric()) & (maindata['Группа'] == 'ПИ101')])
 #print(CEst)
 #print('Общее кол-во оценок:', CEst, 'Кол-во оценок группы ПИ101:', CEst1)
-UN = lipa[lipa['Группа'] == "ПИ101"]
+UN = maindata[lipa['Группа'] == "ПИ101"]
 N = len(UN['Личный номер студента'].unique())
-U = lipa.loc[lipa["Группа"] == "ПИ101", "Личный номер студента"].unique()
+U = maindata.loc[maindata["Группа"] == "ПИ101", "Личный номер студента"].unique()
 #
-SC = lipa['Уровень контроля'].unique()
+SC = maindata['Уровень контроля'].unique()
 #
-date = lipa['Год'].unique()
+date = maindata['Год'].unique()
 #
 print('Общее кол-во оценок:', CEst, '.Кол-во оценок группы ПИ101:', CEst1)
 print("В датасете находятся оценки студентов ПИ101 с следующими личными номерами:", U)
